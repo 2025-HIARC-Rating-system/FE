f16471976934c30f560cee1b752ac0d310d2b579
hojae <ghwo336@naver.com>
2025-02-10 16:01:22 +0900

    feat: 폴더구조변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import {Routes, Route} from "react-router-dom";
 import MainPage from "./page/MainPage";
 import TestPage from "./page/TestPage";
 import DivPage from "./page/DivPage";
-
+import StreakPage from "./page/StreakPage";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/test" element={<TestPage />} />
       <Route path="/div" element={<DivPage />} />
+      <Route path="/streak" element={<StreakPage />} />
     </Routes>
   );
 }
