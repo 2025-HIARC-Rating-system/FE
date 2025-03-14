056922b82486c19c542db1a0b8b2026f5991deaa
이호재 <161708856+ghwo336@users.noreply.github.com>
2025-03-15 01:08:53 +0900

    fix:Search페이지 티어글씨 변경

--- src/components/TierButton.tsx
+++ src/components/TierButton.tsx
@@ -10,6 +10,9 @@ const Button = styled.div<{$tier: number}>`
   white-space: nowrap;
   color: white;
   border-radius: 18px;
+  display: flex;
+  justify-content: center; /* 텍스트를 수평 중앙 정렬 */
+  align-items: center; /* 텍스트를 수직 중앙 정렬 */
 `;
 
 const TierButton = ({tier}: {tier: number}) => {
